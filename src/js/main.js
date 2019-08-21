3072f8b9c8eb9f5ad6df7006b024d6535d3ec097
linkers17 <romeo-gry@mail.ru>
2019-08-21 15:31:47 +0700

    сверстал экран об услугах без адаптива

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+
+   // Подключение slick-слайдера
+/*
+   $('.slider-header').slick({
+
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplay: false,
+      autoplaySpeed: 3000,
+      dots: true,
+      dotsClass: 'slider-header__dots',
+      arrows: false,
+      fade: true,
+      lazyLoad: 'progressive'
+      //appendDots: $('.slider-header__dots')
+
+   });*/
+
+});
